85ace2302f89dd2a721a6b44e2e8f3ef240810fe
SeidC <seidelchristof@googlemail.com>
2024-08-16 14:13:38 +0200

    Update Versoion

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from generator import FileGenerator
 from log import logger
 import time
 __major__ = 0
-__minor__ = 2
+__minor__ = 3
 __patch__ = 0
 
 __version__ = f"{__major__}.{__minor__}.{__patch__}"
