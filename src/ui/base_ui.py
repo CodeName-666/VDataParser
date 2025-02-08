2952c6136deef0a8db0a181764d755d1f1118bb4
SeidC <seidelchristof@googlemail.com>
2025-02-09 00:58:22 +0100

    Implement Market UIs

--- src/ui/base_ui.py
+++ src/ui/base_ui.py
@@ -7,6 +7,6 @@ from PySide6.QtWidgets import QStackedWidget, QWidget
 class BaseUi(QWidget): 
 
 
-    def __init__(self, parent: QStackedWidget = None):
+    def __init__(self, parent: QWidget = None):
         super().__init__(parent)
 
--- src/ui/market.py
+++ src/ui/market.py
@@ -1,6 +1,9 @@
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 from .base_ui import BaseUi
 from .generated import MarketUi
-
+from .market_settings import MarketSetting
+from .data_view import DataView
+from .user_info import UserInfo
 
 
 
@@ -9,4 +12,35 @@ class Market(BaseUi):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.ui = MarketUi()
+        self.marktSettings: MarketSetting = None
+        self.dataView: DataView = None
+        self.userInfo: UserInfo = None
+
+        self.marktSettingsLayout: QVBoxLayout = None
+        self.dataViewLayout : QVBoxLayout = None
+        self.userInfoLayout : QVBoxLayout = None
+
+    def setupUi(self):
         self.ui.setupUi(self)
+
+        self.marktSettingsLayout = self.ui.tab.layout()
+        if self.marktSettingsLayout is None:
+            self.marktSettingsLayout = QVBoxLayout(self.ui.tab)
+            self.marktSettings = MarketSetting(self.marktSettingsLayout)
+            self.marktSettingsLayout.addWidget(self.marktSettings)
+            self.ui.tab.setLayout(self.marktSettingsLayout )
+    
+        self.dataViewLayout = self.ui.tab_2.layout()
+        if self.dataViewLayout is None:
+            self.dataViewLayout = QVBoxLayout(self.ui.tab_2)
+            self.dataView = DataView(self.dataViewLayout)
+            self.dataViewLayout.addWidget(self.dataView)
+            self.ui.tab_2.setLayout(self.dataViewLayout )
+
+        self.userInfoLayout = self.ui.tab_3.layout()
+        if self.userInfoLayout is None:
+            self.userInfoLayout = QVBoxLayout(self.ui.tab_3)
+            self.userInfo = UserInfo(self.userInfoLayout)
+            self.userInfoLayout.addWidget(self.userInfo)
+            self.ui.tab_3.setLayout(self.userInfoLayout )
+    
